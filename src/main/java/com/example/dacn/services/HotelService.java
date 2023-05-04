154e37fc0e03c7f7373eedf9c58a3333414f8c30
lab6TranTrungKien <19130110@st.hcmuaf.edu.vn>
2023-05-04 22:34:06 +0700

    be-hotel-profile

--- src/main/java/com/example/dacn/services/HotelService.java
+++ src/main/java/com/example/dacn/services/HotelService.java
@@ -1,8 +1,22 @@
 package com.example.dacn.services;
 
 import com.example.dacn.model.HotelEntity;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Map;
 
 
 public interface HotelService {
     HotelEntity findById(Long id) throws Exception;
+
+    HotelEntity getHotel(Long id) throws Exception;
+
+    List<HotelEntity> getAllHotel() throws Exception;
+
+    HotelEntity createHotel(HotelEntity hotel) throws Exception;
+
+    ResponseEntity<HotelEntity> updateHotel(Long id, HotelEntity hotel) throws Exception;
+
+    ResponseEntity<Map<String, Boolean>> deleteHotel(Long id) throws Exception;
 }
