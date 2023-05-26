520a3b19a13b19bcdd44d86265a37eae8e1f8a5a
huynhgiahuy0409 <18130094@st.hcmuaf.edu.vn>
2023-05-26 09:23:11 +0700

    .

--- src/main/java/com/example/dacn/responsemodel/RoomResponse.java
+++ src/main/java/com/example/dacn/responsemodel/RoomResponse.java
@@ -1,6 +1,12 @@
 package com.example.dacn.responsemodel;
 
+import com.example.dacn.dto.response.BenefitResponse;
+import com.example.dacn.entity.BenefitEntity;
+import com.example.dacn.entity.DiscountEntity;
+import com.example.dacn.entity.RoomImageEntity;
 import lombok.Data;
+
+import java.util.Set;
 
 @Data
 public class RoomResponse {
@@ -12,4 +18,7 @@ public class RoomResponse {
     private Double originPrice;
     private Double rentalPrice;
     private Double finalPrice;
+    private Set<BenefitResponse> benefits;
+    private DiscountResponse discount;
+    private Set<ImageResponse> roomImages;
 }
