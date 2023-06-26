c3dd2ea237fc2d0b1e7180e0675f737e42c62548
Hieu Nguyen <nhocproaoe@gmail.com>
2023-06-26 11:11:06 +0700

    test

--- src/test/java/com/example/dacn/DacnApplicationTests.java
+++ src/test/java/com/example/dacn/DacnApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.dacn;
 
 import com.example.dacn.dto.response.CartResponse;
-import com.example.dacn.entity.FacilityEntity;
 import com.example.dacn.entity.HotelEntity;
+import com.example.dacn.entity.ProvinceEntity;
 import com.example.dacn.repository.HotelRepository;
-import com.example.dacn.repository.IFacilityRepository;
+import com.example.dacn.responsemodel.FavoriteHotelResponse;
 import com.example.dacn.services.CartService;
+import com.example.dacn.services.impl.FavoriteHotelService;
+import com.example.dacn.services.impl.HotelImageService;
+import com.example.dacn.services.impl.ProvinceService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +30,12 @@ class DacnApplicationTests {
 
     @Autowired
     private CartService cartService;
-
+    @Autowired
+    private HotelImageService hotelImageService;
+    @Autowired
+    private FavoriteHotelService favoriteHotelService;
+    @Autowired
+    private ProvinceService provinceService;
 
     @Test
     public void testListHotel() {
@@ -54,6 +62,26 @@ class DacnApplicationTests {
         String sessionId = "9bdd7086-2cc4-40ad-9e40-3481ae9f16db";
         Iterable<CartResponse> cart = cartService.findBySessionId(sessionId);
         Assertions.assertTrue(cart.spliterator().getExactSizeIfKnown() > 0);
+    }
+
+    @Test
+    public void testFindBanner() {
+        Long hotelId = 1L;
+        String bannerUrl = hotelImageService.findFirstBannerImage(hotelId);
+        Assertions.assertNotNull(bannerUrl);
+    }
+
+    @Test
+    public void testListFavoriteItem() {
+        String username = "tester";
+        Iterable<FavoriteHotelResponse> favoriteList = favoriteHotelService.findAllByUsername(username);
+        Assertions.assertTrue(favoriteList.spliterator().getExactSizeIfKnown() > 0);
+    }
+
+    @Test
+    public void testListProvince() {
+        ProvinceEntity province = provinceService.findBy_domain("dnb");
+        Assertions.assertNotNull(province);
     }
 
 }
